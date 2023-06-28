65cf35f1255465026d537b293dbf76ba6aac9a95
deni <krushitel_950@mail.ru>
2023-06-28 15:03:42 +0300

    сделал анимацию на 1 сцену

--- components/SeriesOne/OneScene.js
+++ components/SeriesOne/OneScene.js
@@ -1,39 +1,79 @@
-import {Image, ImageBackground, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import { useEffect, useRef } from "react";
+import {
+  Animated,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function OneScene() {
-    return (
-        <SafeAreaView style={styles.wrapper}>
-            <View style={styles.container}>
-                <ImageBackground style={styles.backgroundImg} source={require('../../assets/scene/1/1.png')}/>
-                <Text style={styles.dialog}>
-                    МНЕ СНЯТСЯ СЧАСТЛИВЫЕ СНЫ. В НИХ НЕБО ЧИСТОЕ И НЕ ЗАПЯТНАНО ГРЯЗНЫМ
-                    ДЫХАНИЕМ ГОРОДА.
-                </Text>
-            </View>
-        </SafeAreaView>
-    );
+  const fadeAnimScale = useRef(new Animated.Value(1)).current;
+
+  const animScale = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.sequence([
+      Animated.timing(fadeAnimScale, {
+        toValue: 3,
+        duration: 10000, 
+        delay: 500,
+        useNativeDriver: false,
+      }),
+      Animated.timing(fadeAnimScale, {
+        toValue: 1,
+        duration: 5000,
+        useNativeDriver: false,
+      }),
+    ]).start((event) => {
+      if (event.finished) animScale();
+    });
+  };
+
+  useEffect(() => {
+    animScale();
+  });
+
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Animated.Image
+          style={[
+            styles.backgroundImg,
+            { transform: [{ scale: fadeAnimScale }] },
+          ]}
+          source={require("../../assets/scene/1/1.png")}
+        />
+        <Text style={styles.dialog}>
+          МНЕ СНЯТСЯ СЧАСТЛИВЫЕ СНЫ. В НИХ НЕБО ЧИСТОЕ И НЕ ЗАПЯТНАНО ГРЯЗНЫМ
+          ДЫХАНИЕМ ГОРОДА.
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        width: '100%',
-        height: '100%'
-    },
-    container:{
-        width: '100%',
-        height: '100%',
-    },
-    dialog: {
-        top: '20%',
-        left: '35%',
-        width: '30%',
-        position: "absolute",
-        backgroundColor: 'white',
-        textAlign: 'center',
-        borderRadius: 5,
-    },
-    backgroundImg: {
-        width: '100%',
-        height: '100%'
-    }
+  wrapper: {
+    width: "100%",
+    height: "100%",
+    overflow: "hidden",
+  },
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+  dialog: {
+    top: "20%",
+    left: "35%",
+    width: "30%",
+    position: "absolute",
+    backgroundColor: "white",
+    textAlign: "center",
+    borderRadius: 5,
+  },
+  backgroundImg: {
+    width: "100%",
+    height: "100%",
+  },
 });
--- components/SeriesOne/TwoScene.js
+++ components/SeriesOne/TwoScene.js
@@ -1,74 +1,146 @@
-import {Image, ImageBackground, SafeAreaView, StyleSheet, Text, View} from 'react-native';
-
+import { useEffect, useRef } from "react";
+import {
+  Animated,
+  Dimensions,
+  Image,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function TwoScene() {
-    return (
-        <SafeAreaView style={styles.wrapper}>
-            <View style={styles.container}>
-                <ImageBackground
-                    style={styles.backgroundImg}
-                    source={require('../../assets/scene/2/2.png')}
-                />
-                <Image
-                    source={require('../../assets/scene/2/obl/1.png')}
-                    style={styles.obl}
-                />
-                <Image
-                    source={require('../../assets/scene/2/obl/2.png')}
-                    style={styles.obl}
-                />
-                <Image
-                    source={require('../../assets/scene/2/obl/3.png')}
-                    style={styles.obl}
-                />
-                <Image
-                    source={require('../../assets/scene/2/obl/4.png')}
-                    style={styles.obl}
-                />
-                <Image
-                    source={require('../../assets/scene/2/or.png')}
-                    style={styles.orel}
-                />
-                <Text style={styles.dialog}>
-                    ОГРОМНЫЕ ЗЕЛЁНЫЕ РАВНИНЫ ВСТРЕЧАЮТСЯ С МОГУЧИМИ ГОРНЫМИ ХРЕБТАМИ.
-                </Text>
-            </View>
-        </SafeAreaView>
-    );
+  const fadeAnimScale = useRef(new Animated.Value(1)).current;
+  const fadeOrelTranslateX = useRef(new Animated.Value(0)).current;
+  const fadeOrelTranslateY = useRef(new Animated.Value(0)).current;
+
+  const animTranslateX = () => {
+    Animated.sequence([
+      Animated.timing(fadeOrelTranslateX, {
+        toValue: Dimensions.get("window").width,
+        duration: 5000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(fadeOrelTranslateX, {
+        toValue: Dimensions.get("window").width + 100,
+        duration: 20000,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+  const animTranslateY = () => {
+    Animated.sequence([
+      Animated.timing(fadeOrelTranslateY, {
+        toValue: Dimensions.get("window").height,
+        duration: 5000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(fadeOrelTranslateY, {
+        toValue: Dimensions.get("window").height + 100,
+        duration: 20000,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+
+  const animScale = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.sequence([
+      Animated.timing(fadeAnimScale, {
+        toValue: 1.2,
+        duration: 10000,
+        delay: 500,
+        useNativeDriver: false,
+      }),
+      Animated.timing(fadeAnimScale, {
+        toValue: 1,
+        duration: 5000,
+        useNativeDriver: false,
+      }),
+    ]).start((event) => {
+      if (event.finished) animScale();
+    });
+  };
+
+  useEffect(() => {
+    animScale();
+    animTranslateX();
+    animTranslateY();
+  });
+
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.backgroundImg}
+          source={require("../../assets/scene/2/2.png")}
+        />
+        <Animated.Image
+          source={require("../../assets/scene/2/obl/1.png")}
+          style={[styles.obl, { transform: [{ scale: fadeAnimScale }] }]}
+        />
+        <Animated.Image
+          source={require("../../assets/scene/2/obl/2.png")}
+          style={[styles.obl, { transform: [{ scale: fadeAnimScale }] }]}
+        />
+        <Animated.Image
+          source={require("../../assets/scene/2/obl/3.png")}
+          style={[styles.obl, { transform: [{ scale: fadeAnimScale }] }]}
+        />
+        <Animated.Image
+          source={require("../../assets/scene/2/obl/4.png")}
+          style={[styles.obl, { transform: [{ scale: fadeAnimScale }] }]}
+        />
+        <Animated.Image
+          source={require("../../assets/scene/2/or.png")}
+          style={[styles.orel, { right: fadeOrelTranslateX, bottom: fadeOrelTranslateY }]}
+        />
+        <Text style={styles.dialog}>
+          ОГРОМНЫЕ ЗЕЛЁНЫЕ РАВНИНЫ ВСТРЕЧАЮТСЯ С МОГУЧИМИ ГОРНЫМИ ХРЕБТАМИ.
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        width: '100%',
-        height: '100%',
-    },
-    container:{
-        width: '100%',
-        height: '100%',
-    },
-    dialog: {
-        top: '20%',
-        left: '35%',
-        width: '30%',
-        position: "absolute",
-        backgroundColor: 'white',
-        textAlign: 'center',
-        borderRadius: 5,
-    },
-    backgroundImg: {
-        width: '100%',
-        height: '100%',
-
-    },
-    obl: {
-
-    },
-    orel: {
-        zIndex: 1,
-        position: 'absolute',
-        bottom: '20%',
-        right: '20%',
-        width: '40%',
-        height: '57%',
-    }
+  wrapper: {
+    width: "100%",
+    height: "100%",
+    overflow: "hidden",
+  },
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+  dialog: {
+    top: "20%",
+    left: "35%",
+    width: "30%",
+    position: "absolute",
+    backgroundColor: "white",
+    textAlign: "center",
+    borderRadius: 5,
+    zIndex: 4,
+  },
+  backgroundImg: {
+    width: "100%",
+    height: "100%",
+  },
+  obl: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    zIndex: 2,
+  },
+  orel: {
+    zIndex: 1,
+    position: "absolute",
+    bottom: "20%",
+    right: "20%",
+    resizeMode: "contain",
+    width: "40%",
+    height: "60%",
+  },
 });
